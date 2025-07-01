7439c52fce2f2f2f9eea21f93da9c2c1b542f7b9
jithendrareddyega <119729143+jithendrareddyega@users.noreply.github.com>
2025-07-01 14:11:50 +0530

    Delete Assignment2 directory

--- Assignment2/HospitalManagement/records/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Patient
-
-class PatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = ['name', 'age', 'contact', 'diagnosis', 'admission_date']  # Correct fields from your model
